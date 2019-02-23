751782d18e5aaa65bf3334f65438b083931f8b50
Zhen Li <zhen@cs.toronto.edu>
2019-02-23 18:55:54 -0500

    Songs.html done

--- javascripts/script_words.js
+++ javascripts/script_words.js
@@ -35,10 +35,21 @@ $(document).ready(function()
                 break;
             case 'Escape':
                 // resume the status from runout / timeout
-                if (currGroup < 3) {
+                if (currGroup < groupNum - 1) {
+                    // Log the groupScores
+                    groupScores[currGroup] = corrCnt;
+
                     currGroup += 1;
                     InitEachGroup();
                     return;
+                }
+                else {
+                    groupScores[currGroup] = corrCnt;
+
+                    // END
+                    $('#curr_word').html("游戏结束");
+                    $('#word_no').html(`1 ~ ${currGroup + 1}号队伍得分: ` + groupScores.join());
+                    $('#curr_song').html("");
                 }
                 break;
             default:
@@ -73,20 +84,18 @@ function Init()
 {
     words = "清华科技园;辅导员;一带一路;水木清华;元宵;北平市;《无问西东》;丁香花;汤圆;光华管理学院;猛龙队;财源广进;北京电影学院;《变形记》;拜年;东区浴室;细思极恐;福星高照;自动控制;静斋;西门烤翅;度假;朱自清;万人食堂;七十周年;综合体育馆;二维码;清华学堂;荷塘;基因编辑;博士后;工字厅;清华附中;照澜院;卧伍真;成府路;人艰不拆;辞旧迎新;小猪佩奇;新斋;艺术团;年年有余;图书馆;新竹;守岁;《金蛇狂舞》;近春园;金玉满堂;微波炉;金工实习;麻辣烫;校庆;梅竹赛;5G;猜灯谜;大类招生;年夜饭;观畴园;九字班;未名湖;情人坡;学生会;大吉大利;学生节;滑溜里脊;《飞驰人生》;科学馆;古月堂;《四世同堂》;麻辣香锅;清华园;酒井;荒岛;毕业证;日晷;高尔夫;校园卡;期末考试;《流浪地球》;昆明湖;葡萄;桥牌;学生证;马约翰;冻雨;《红楼梦》;团委;CN Tower;桃李园;二校门;奖学金;拓扑学;累觉不爱;梅花;《知否知否》;甲所;紫荆花;蒸蒸日上;压岁钱;西大饭厅;对联;社团;《大江大河》;博雅塔;昆明;紫荆公寓;共享单车;电动车;北冰洋;酱肘子;运动会;外星人;玉兔二号;除夕;工会俱乐部;滑雪;五道口;同方部;微积分;大礼堂";
     wordList = words.split(';');
-    groupNum = 4;
+    groupNum = 3;
     wordNumEach = Math.floor(wordList.length / groupNum);
     console.log(wordNumEach);
-    wordGroup = [wordList.slice(0, wordNumEach), wordList.slice(wordNumEach, wordNumEach * 2), wordList.slice(wordNumEach * 2, wordNumEach * 3), wordList.slice(wordNumEach * 3, wordNumEach * 4)];
+    // wordGroup = [wordList.slice(0, wordNumEach), wordList.slice(wordNumEach, wordNumEach * 2), wordList.slice(wordNumEach * 2, wordNumEach * 3), wordList.slice(wordNumEach * 3, wordNumEach * 4)];
+    wordGroup = [wordList.slice(0, wordNumEach), wordList.slice(wordNumEach, wordNumEach * 2), wordList.slice(wordNumEach * 2, wordNumEach * 3)];
     console.log(wordGroup);
     currGroup = 0
-    groupScores = [0, 0, 0, 0];
-
+    groupScores = [0, 0, 0];
 
     currStatus = GameStatus.DESC;
 
     document.getElementById("words").style.display = 'none';
-    // TODO: add rules page as page -1.
-    // BUG: start from 1.
 }
 
 function GameStart()
@@ -102,7 +111,7 @@ function GameStart()
 function InitEachGroup()
 {
     // Debug; should be 90
-    currTimer = 90;
+    currTimer = 150;
     wordID = -1;
     corrCnt = 0;
     passCnt = 0;
