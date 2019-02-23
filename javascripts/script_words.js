f147b990c83e6c76adad42e01ad9f150464c52f5
Zhen Li <zhen@cs.toronto.edu>
2019-02-23 15:30:59 -0500

    finish words section

--- javascripts/script_words.js
+++ javascripts/script_words.js
@@ -1,3 +1,10 @@
+var GameStatus = {
+    DESC: 1,
+    READY: 2,
+    RUNNING: 3,
+    END: 4,
+};
+
 $(document).ready(function()
 {
     Init();
@@ -8,110 +15,123 @@ $(document).ready(function()
 
         switch (keyName) {
             case 'Enter':
-                if (started && !runout) {
-                    // Go to next one.
-                    corrCnt += 1;
+                if (currStatus == GameStatus.DESC) {
+                    // Start Game: DESC -> READY
+                    GameStart();
                 }
-                else if (!started) {
+                else if (currStatus == GameStatus.READY) {
+                    // Ready -> game start
+                    currStatus = GameStatus.RUNNING;
+                    // First word init (from -1)
+                    NextWord();
                     // Start timer.
-                    InitEachGroup()
-
-                    $('#curr_word').html(wordGroup[nextGroup][wordID]);
-                    started = true;
-
-                    runCounter();
+                    runTimer();
+                }
+                else if (currStatus == GameStatus.RUNNING) {
+                    // Next word. Log successful word.
+                    corrCnt += 1;
+                    NextWord();
                 }
                 break;
             case 'Escape':
                 // resume the status from runout / timeout
-                if (nextGroup < 4) {
-                    nextGroup += 1;
+                if (currGroup < 3) {
+                    currGroup += 1;
                     InitEachGroup();
                     return;
                 }
                 break;
             default:
-                if (!runout) {
+                // any other character during the game
+                if (currStatus == GameStatus.RUNNING) {
                     passCnt += 1;
+                    NextWord();
                 }
                 break;
         }
-
-        if (!runout) {
-            wordID += 1;
-            $('#team_info').html(`team ${nextGroup + 1}: correct (${corrCnt}), pass (${passCnt})`);
-
-            if (wordID < wordGroup[nextGroup].length) {
-                $('#curr_word').html(wordGroup[nextGroup][wordID]);
-            }
-            else {
-                $('#curr_word').html("词库用完啦！");
-                runout = true;
-            }
-        }
-
     });
 });
 
+function  NextWord()
+{
+    // Update team score info and display the next word
+    $('#team_info').html(`${currGroup + 1}号队伍: 正确 (${corrCnt}), 跳过 (${passCnt})`);
+
+    wordID += 1;
+    if (wordID < wordGroup[currGroup].length) {
+        $('#curr_word').html(wordGroup[currGroup][wordID]);
+        $('#word_no').html(`${wordID + 1} / ${wordGroup[currGroup].length}`);
+    }
+    else {
+        $('#curr_word').html(`词库用完啦！<br>${currGroup + 1}号队伍：${corrCnt}分`);
+        // runout = true;
+        currStatus = GameStatus.END;
+    }
+}
+
 function Init()
 {
-    words = "大礼堂;运动会;九字班;清华学堂;猜灯谜;照澜院;毕业证;西大饭厅;图书馆;无问西东;万人食堂;昆明湖;博士后;东区浴室;同方部;酒井;团委;麻辣香锅;元宵;博雅塔;观畴园;新斋;高尔夫;七十周年;麻辣烫;北冰洋;汤圆;自动控制;5G;二维码;葡萄;校园卡;外星人;静斋;金工实习;工字厅;艺术团;大类招生;未名湖;近春园;微波炉;滑溜里脊;学生节;工会俱乐部;清华科技园;成府路;紫荆花;玉兔二号;朱自清;期末考试;马约翰;情人坡;二校门;昆明;科学馆;水木清华;体育馆;流浪地球;新竹;酱肘子;辅导员;卧伍真;飞驰人生;梅竹赛;丁香花;日晷;清华园;电动车;北平市;荷塘;社团;古月堂;紫荆公寓;西门烤翅;奖学金;梅花;北京电影学院;荒岛;共享单车;光华管理学院;桥牌;学生会;学生证;五道口;清华附中;甲所;微积分;桃李园";
+    words = "清华科技园;辅导员;一带一路;水木清华;元宵;北平市;《无问西东》;丁香花;汤圆;光华管理学院;猛龙队;财源广进;北京电影学院;《变形记》;拜年;东区浴室;细思极恐;福星高照;自动控制;静斋;西门烤翅;度假;朱自清;万人食堂;七十周年;综合体育馆;二维码;清华学堂;荷塘;基因编辑;博士后;工字厅;清华附中;照澜院;卧伍真;成府路;人艰不拆;辞旧迎新;小猪佩奇;新斋;艺术团;年年有余;图书馆;新竹;守岁;《金蛇狂舞》;近春园;金玉满堂;微波炉;金工实习;麻辣烫;校庆;梅竹赛;5G;猜灯谜;大类招生;年夜饭;观畴园;九字班;未名湖;情人坡;学生会;大吉大利;学生节;滑溜里脊;《飞驰人生》;科学馆;古月堂;《四世同堂》;麻辣香锅;清华园;酒井;荒岛;毕业证;日晷;高尔夫;校园卡;期末考试;《流浪地球》;昆明湖;葡萄;桥牌;学生证;马约翰;冻雨;《红楼梦》;团委;CN Tower;桃李园;二校门;奖学金;拓扑学;累觉不爱;梅花;《知否知否》;甲所;紫荆花;蒸蒸日上;压岁钱;西大饭厅;对联;社团;《大江大河》;博雅塔;昆明;紫荆公寓;共享单车;电动车;北冰洋;酱肘子;运动会;外星人;玉兔二号;除夕;工会俱乐部;滑雪;五道口;同方部;微积分;大礼堂";
     wordList = words.split(';');
     groupNum = 4;
-    wordGroup = [wordList.slice(0,22), wordList.slice(22,44), wordList.slice(44,66), wordList.slice(66,88)];
+    wordNumEach = Math.floor(wordList.length / groupNum);
+    console.log(wordNumEach);
+    wordGroup = [wordList.slice(0, wordNumEach), wordList.slice(wordNumEach, wordNumEach * 2), wordList.slice(wordNumEach * 2, wordNumEach * 3), wordList.slice(wordNumEach * 3, wordNumEach * 4)];
     console.log(wordGroup);
-    nextGroup = 0
-    started = false;
+    currGroup = 0
+    groupScores = [0, 0, 0, 0];
 
+
+    currStatus = GameStatus.DESC;
+
+    document.getElementById("words").style.display = 'none';
     // TODO: add rules page as page -1.
-    // BUG: start from 1. 
+    // BUG: start from 1.
+}
+
+function GameStart()
+{
+    currStatus = GameStatus.READY;
+    // Remove description
+    document.getElementById("description").style.display = 'none';
+    document.getElementById("words").style.display = 'block';
 
     InitEachGroup();
 }
 
 function InitEachGroup()
 {
-    currTimer = 10;
-    wordID = 0;
+    // Debug; should be 90
+    currTimer = 90;
+    wordID = -1;
     corrCnt = 0;
     passCnt = 0;
-    runout = false;
-    timeout = false;
+    // runout = false;
+    // timeout = false;
 
+    currStatus = GameStatus.READY;
     $('#curr_word').html("Ready?");
-    $('#timer').html('Timer: ' + currTimer)
-    $('#team_info').html(`team ${nextGroup + 1}: correct (${corrCnt}), pass (${passCnt})`);
+    $('#word_no').html('-- / --');
+    $('#timer').html('剩余时间: ' + currTimer)
+    $('#team_info').html(`${currGroup + 1}号队伍: 正确 (${corrCnt}), 跳过 (${passCnt})`);
 }
 
-function runCounter() {
-    $('#timer').html('Timer: ' + currTimer)
+function runTimer() {
+    $('#timer').html('剩余时间: ' + currTimer)
     currTimer -= 1;
     if (currTimer >= 0) {
-        if (runout) {
+        if (currStatus != GameStatus.RUNNING) {
             // End immediately
             return;
         }
         else {
-            setTimeout(runCounter, 1000);
+            setTimeout(runTimer, 1000);
         }
     }
     else {
-        $('#curr_word').html(`${nextGroup + 1}号队伍：${corrCnt}分`);
-        started = false;
-        timeout = true;
-
-        // Same for "OK" and "Cancel"
-        if (confirm("Time out")) {
-            if (nextGroup == 3)
-            {
-                $('#curr_word').html("游戏结束");
-            }
-        }
-        else {
-            if (nextGroup == 3)
-            {
-                $('#curr_word').html("游戏结束");
-            }
-        }
+        $('#curr_word').html(`时间到^-^<br>${currGroup + 1}号队伍：${corrCnt}分`);
+        // started = false;
+        // timeout = true;
+        currStatus = GameStatus.END;
     }
 }
