c0a4969977fcd6a5bdb00d73f29d2b675aa9ac69
Zhen Li <zhen@cs.toronto.edu>
2019-02-23 23:55:10 -0500

    minor fix

--- javascripts/script_sports.js
+++ javascripts/script_sports.js
@@ -1,3 +1,31 @@
 function GoBack() {
     top.location.href = "index.html";
 }
+
+function ToggleHit(player, num) {
+    newScore = 0;
+    if (team_scores[player][num] > 0) {
+        newScore = 0;
+    }
+    else {
+        if (num == 0) {
+            newScore = 10;
+        }
+        else if (num == 3) {
+            newScore = 20;
+        }
+        else {
+            newScore = 5;
+        }
+    }
+
+    team_scores[player][num] = newScore;
+    
+}
+
+$(document).ready(function()
+{
+    console.log("init-sports");
+
+    team_scores = [[0,0,0,0], [0,0,0,0], [0,0,0,0]];
+});
