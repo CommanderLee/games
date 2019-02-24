d9f71c66d733fa34a811d8c49323afd246c239b3
Zhen Li <zhen@cs.toronto.edu>
2019-02-23 22:14:12 -0500

    test load

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,5 @@
 window.onload = function() {
-    console.log('Page loaded');
-};
-
-$(document).ready(function()
-{
-    console.log("init-main");
+    console.log('Page loaded - main');
 
     Init();
 
@@ -46,6 +41,52 @@ $(document).ready(function()
         }
 
     });
+};
+
+$(document).ready(function()
+{
+    console.log("init-main");
+
+    // Init();
+    //
+    // $('#title').click(function(e)
+    // {
+    //     Init();
+    // });
+    //
+    // $('ul.ul_menu li').click(function(e)
+    // {
+    //     map = e.target.id;
+    //     name = "Default";
+    //     // Hide menu
+    //     document.getElementById("menu").style.display = "none";
+    //     switch(map) {
+    //         case "words":
+    //             words = "大礼堂;运动会;九字班;清华学堂;猜灯谜;照澜院;毕业证;西大饭厅;图书馆;无问西东;万人食堂;昆明湖;博士后;东区浴室;同方部;酒井;团委;麻辣香锅;元宵;博雅塔;观畴园;新斋;高尔夫;七十周年;麻辣烫;北冰洋;汤圆;自动控制;5G;二维码;葡萄;校园卡;外星人;静斋;金工实习;工字厅;艺术团;大类招生;未名湖;近春园;微波炉;滑溜里脊;学生节;工会俱乐部;清华科技园;成府路;紫荆花;玉兔二号;朱自清;期末考试;马约翰;情人坡;二校门;昆明;科学馆;水木清华;体育馆;流浪地球;新竹;酱肘子;辅导员;卧伍真;飞驰人生;梅竹赛;丁香花;日晷;清华园;电动车;北平市;荷塘;社团;古月堂;紫荆公寓;西门烤翅;奖学金;梅花;北京电影学院;荒岛;共享单车;光华管理学院;桥牌;学生会;学生证;五道口;清华附中;甲所;微积分;桃李园";
+    //             wordList = words.split(';');
+    //             groupNum = 4;
+    //             wordGroup = [wordList.slice(0,22), wordList.slice(22,44), wordList.slice(44,66), wordList.slice(66,88)];
+    //             console.log(wordGroup);
+    //
+    //             name = "Ready?";
+    //             document.getElementById("words").style.display = "block";
+    //
+    //             break;
+    //         case "lottery":
+    //             // Show actual content
+    //             document.getElementById("lottery_detail").style.display = "block";
+    //
+    //             // $('#description').html("注释：爆破兵营地一刷新就去占领，尽可能多地占领地图下半部分的营地。");
+    //             // counterInit = 125;
+    //             // rangeInit = 15;
+    //             // counterNext = 125;
+    //             // rangeNext = 15;
+    //             // countdown_time.innerText = "2 : 5";
+    //             // mapInfoInit();
+    //             break;
+    //     }
+    //
+    // });
 });
 
 function Init()
